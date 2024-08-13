5e966720dc134d6ac447b78dbb01994ffdc3860e
Yoonseoparkk <patpark97@gmail.com>
2024-08-13 17:24:53 +0900

    feat: [박윤서] 카카오 이메일 중복 체크 [ATTF-9]

--- src/kakaoAuthentication/redirection/KakaoRedirection.vue
+++ src/kakaoAuthentication/redirection/KakaoRedirection.vue
@@ -12,6 +12,7 @@ export default {
     methods: {
         ...mapActions(kakaoAuthenticationModule,
         ['requestAccessTokenToDjangoRedirection', 'requestKakaoUserInfoToDjango']),
+        ...mapActions(accountModule, ['requestEmailDuplicationCheckToDjango']),
 
         async setRedirectData () {
             const code = this.$route.query.code
@@ -21,9 +22,20 @@ export default {
             const email = userInfo.kakao_account.email
             console.log('userInfo:', userInfo)
             console.log('email:', email)
+
+            const isEmailDuplication = await this.requestEmailDuplicationCheckToDjango()
+
+            if (isEmailDuplication === true) {
+                console.log('기존 가입 고객입니다.')
+                const accessToken = localStorage.getItem("accessToken")
+
+                this.$router.push('/')
+            } else {
+                console.log('신규 가입 고객입니다.')
+                this.$router.push('/account/kakao-register')
+            }
         },
     },
-
     async created () {
             await this.setRedirectData()
         }
