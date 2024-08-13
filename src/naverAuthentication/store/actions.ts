26b3f155aa22ef54a8480de4d0421010ff1ee478
Yoonseoparkk <patpark97@gmail.com>
2024-08-13 17:02:02 +0900

    feat: [박윤서] 네이버 계정 정보 요청 [ATTF-18]

--- src/naverAuthentication/store/actions.ts
+++ src/naverAuthentication/store/actions.ts
@@ -6,6 +6,7 @@ import axiosInst from "@/utility/axiosInstance"
 export type AuthenticationActions = {
     requestNaverOauthRedirectionToDjango(): Promise<void>
     requestAccessTokenToDjangoRedirection(context: ActionContext<NaverAuthenticationState, any>, payload: { code: string }): Promise<void>
+    requestNaverUserInfoToDjango(context: ActionContext<NaverAuthenticationState, any>): Promise<any>
 }
 
 const actions: AuthenticationActions = {
@@ -29,6 +30,21 @@ const actions: AuthenticationActions = {
             localStorage.setItem("accessToken", response.data.accessToken.access_token)
         } catch (error) {
             console.log('Access Token 요청 중 문제 발생:', error)
+            throw error
+        }
+    },
+    async requestNaverUserInfoToDjango(
+        context: ActionContext<NaverAuthenticationState, any>): Promise<any> {
+        try {
+            const accessToken = localStorage.getItem("accessToken")
+            const userInfoResponse: AxiosResponse<any> =
+                await axiosInst.djangoAxiosInst.post('/naver_oauth/naver/user-info', { access_token: accessToken })
+
+            const userInfo = userInfoResponse.data.user_info
+
+            return userInfo
+        } catch (error) {
+            alert('사용자 정보 가져오기 실패!')
             throw error
         }
     },
