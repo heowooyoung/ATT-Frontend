fac6ec6a1531dae68699c36c184f808257467bf7
Yoonseoparkk <patpark97@gmail.com>
2024-08-14 14:49:26 +0900

    style: [박윤서] 유니티 화면 비율 조정 [ATTF-21]

--- src/components/UnityGame.vue
+++ src/components/UnityGame.vue
@@ -15,12 +15,13 @@ export default {
   display: flex;
   justify-content: center; /* 가로 중앙 정렬 */
   align-items: center; /* 세로 중앙 정렬 */
-  height: 100vh; /* 화면 전체 높이를 사용 */
+  margin-top: 35px;
+  height: 605px;
 }
 
 .unity-frame {
-  width: 50%; /* 필요에 따라 조정 가능 50으로하니 가운데로 옴*/ 
-  height: 80vh; /* 필요에 따라 조정 가능 */
+  width: 96%;
+  height: 100%;
   border: none;
 }
 </style>
