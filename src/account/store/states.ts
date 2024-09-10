bc351b948a0d1476b4fb0675196fe6a59a9e6e9e
heowooyoung <120430842+heowooyoung@users.noreply.github.com>
2024-09-10 12:40:44 +0900

    fix: [허우영] 관리자 등록을 위한 Vuex에 등록 추가 [ATTF-28]

--- src/account/store/states.ts
+++ src/account/store/states.ts
@@ -1,15 +1,27 @@
 export interface Account {
-    nickname: string,
+    nickname: string;
+}
+
+export interface User {
+    id: number;
+    email: string;
+    nickname: string;
+    name: string;
+    gender: string;
+    mbti: string;
+    loginType: string;
 }
 
 export interface AccountState {
-    isLoggedIn: boolean,
-    account: Account | null
+    isLoggedIn: boolean;
+    account: Account | null;
+    users: User[]; 
 }
 
 const state: AccountState = {
     isLoggedIn: false,
-    account: null
-}
+    account: null,
+    users: [],  
+};
 
-export default state
+export default state;
